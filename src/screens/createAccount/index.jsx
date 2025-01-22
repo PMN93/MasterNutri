5791617ee255a14a6977303093b4d639843cd6e6
Pedro Neto <pedroneto@pecege.com>
2025-01-22 11:13:13 -0300

    Tela Inicial atualizada

--- src/screens/createAccount/index.jsx
+++ src/screens/createAccount/index.jsx
@@ -6,12 +6,12 @@ const SignUp = () => {
   const [isAgree, setIsAgree] = useState(false);
 
   return (
-    <div className="flex flex-col w-screen h-screen justify-center items-center">
+    <div className="flex flex-col w-screen h-screen justify-center items-center pt-10 pb-5">
       <img className="w-48 h-48" src={mascote}></img>
       <div className="flex flex-col mt-2 items-center">
         <h1 className="text-3xl font-bold">Criar uma conta</h1>
       </div>
-      <div className="flex flex-col w-4/12 mt-8">
+      <div className="flex flex-col w-96 mt-8">
         <input
           className="border border-gray-300 p-2 mb-1 rounded-md"
           type="text"
@@ -23,15 +23,15 @@ const SignUp = () => {
           placeholder="Confirme seu email"
         ></input>
         <input
-          className="border border-gray-300 mb-8 p-2 rounded-md"
+          className="border border-gray-300 mb-12 p-2 rounded-md"
           type="password"
           placeholder="Crie uma senha"
         ></input>
       </div>
-      <div className="flex rounded-md bg-orange-200 items-center justify-center p-1 mb-2">
+      <div className="flex rounded-md bg-orange-200 items-center justify-center p-1 mb-5">
         Para instruções e contato
       </div>
-      <div className="flex w-4/12 border border-gray-300 mb-3 p-2 rounded-md gap-2">
+      <div className="flex w-96 border border-gray-300 mb-3 p-2 rounded-md gap-2">
         <img src={zap} className="w-6"></img>
         <input
           type="text"
@@ -40,7 +40,7 @@ const SignUp = () => {
         ></input>
       </div>
 
-      <div className="flex flex-row mt-6 gap-2">
+      <div className="flex flex-row w-96 flex-wrap mt-6 gap-2 items-center justify-center">
         {isAgree == false ? (
           <div
             className="flex w-5 h-5 border-solid border-2 border-orange-500 cursor-pointer"
@@ -70,13 +70,13 @@ const SignUp = () => {
         </a>
         <p>da Empresa Tal.</p>
       </div>
-      <div className="flex flex-col w-4/12 mt-8">
+      <div className="flex flex-col w-96 mt-8">
         <button className="bg-orange-500 text-white p-2 rounded-md">
           CRIAR CONTA
         </button>
       </div>
 
-      <div className="flex flex-row mt-8 gap-2">
+      <div className="flex flex-row mt-8 gap-2 mb-8">
         <button className="rounded-md p-2 text-orange-500 bg-transparent border-solid border-2 border-orange-500 font-bold ">
           ENTRAR COM CONTA
         </button>
--- src/screens/initPage/index.jsx
+++ src/screens/initPage/index.jsx
@@ -1,52 +1,82 @@
 import React, { useState } from "react";
 import mascote from "../../assets/cenoura.png";
-import { ArrowRightStartOnRectangleIcon, Bars4Icon, XMarkIcon } from "@heroicons/react/16/solid";
+import zap from "../../assets/zap.png";
+import {
+  ArrowRightStartOnRectangleIcon,
+  Bars4Icon,
+  XMarkIcon,
+} from "@heroicons/react/16/solid";
 
 const InitPage = () => {
   const [openBar, setOpenBar] = useState(false);
 
   return (
-    <div className={openBar == true ? "flex flex-col w-auto h-auto overflow-hidden relative bg-blue-50" : "flex flex-col w-auto h-auto overflow-hidden relative" }>
-    <div className="flex w-screen h-16 border items-center px-5">
-      <button
-        onClick={() => {
-          setOpenBar(true);
+    <div
+      className={
+        openBar == true
+          ? "flex flex-col w-auto h-screen overflow-auto overflow-x-hidden relative bg-blue-50"
+          : "flex flex-col w-auto h-screen overflow-auto overflow-x-hidden relative"
+      }
+    >
+      <div className="flex w-screen min-h-14 border items-center px-5">
+        <button
+          onClick={() => {
+            setOpenBar(true);
           }}
           className="left-5 z-40"
-      >
-        <Bars4Icon className="w-6 h-6 text-orange-500" />
-      </button>
-      <div className="flex flex-row w-full items-center justify-center left-1/2 transform -translate-x-1/2 absolute">
-        <div className="font-semibold text-2xl text-orange-500">
-          Master Nutri
-        </div>
-        <div>
-          <img className="w-14" src={mascote}></img>
-        </div>
+        >
+          <Bars4Icon className="w-6 h-6 text-orange-500" />
+        </button>
+        <div className="flex flex-row w-full items-center justify-center left-1/2 transform -translate-x-1/2 absolute">
+          <div className="font-semibold text-2xl text-orange-400">
+            Master Nutri
+          </div>
+          <div>
+            <img className="w-14" src={mascote}></img>
+          </div>
         </div>
       </div>
-      <div className="flex w-screen h-8 items-center justify-center bg-orange-200 shadow-sm">
+      <div className="flex w-screen h-8 items-center justify-center bg-orange-500 shadow-sm text-white">
         Preencha suas medidas abaixo ↓
       </div>
-      {/* <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">1</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">2</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">3</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">4</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">5</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">6</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">7</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">8</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">1</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">2</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">3</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">4</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">5</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">6</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">7</div>
-      <div className="w-screen min-h-28 bg-red-50 mb-10 border border-black">8</div> */}
+
+      <div className="flex flex-col w-screen items-center justify-center">
+        <div className="flex pr-16 pl-16 pt-3 pb-3 w-auto h-auto border border-gray-200 rounded-[1.2rem] mt-2 shadow-sm items-center justify-center flex-wrap space-x-40">
+          <div className="flex flex-col items-center">
+            🍑<p>Ver Dieta</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <img className="w-5" src={zap}></img>
+            <p>Suporte</p>
+          </div>
+        </div>
+
+        <div className="flex flex-col p-5 md:min-w-[50rem] w-96 h-auto border border-gray-200 rounded-[1.2rem] mt-2 shadow-sm items-center justify-center bg-slate-200">
+          <div className="flex w-full h-8 border border-gray-200 rounded-[1.2rem] shadow-sm bg-gray-50 mb-5">
+            <div className="flex w-[9rem] h-8 border-r-2 border-gray-200 rounded-[1.2rem] bg-orange-500 text-white items-center justify-center">
+              Medidas
+            </div>
+          </div>
+          <input
+            className="border border-gray-300 p-2 rounded-md w-full mb-4"
+            type="number"
+            placeholder="Peso"
+          ></input>
+          <input
+            className="border border-gray-300 p-2 rounded-md w-full mb-4"
+            type="number"
+            placeholder="Altura"
+          ></input>
+          <input
+            className="border border-gray-300 p-2 rounded-md w-full mb-4"
+            type="number"
+            placeholder="Idade"
+          ></input>
+        </div>
+      </div>
 
       {openBar == true ? (
-        <div className="overflow-x-hidden h-svh bg-white  border font-bold left-0 top-0 z-50 rounded-lg shadow-lg p-4 absolute">    
+        <div className="overflow-x-hidden h-screen bg-white  border font-bold left-0 top-0 z-50 rounded-lg shadow-lg p-4 absolute">
           <button
             onClick={() => {
               setOpenBar(false);
@@ -83,13 +113,14 @@ const InitPage = () => {
           </div>
           <div className="w-full border-b-2 mt-20"></div>
           <div className="flex text-base text-center w-full mt-8 mb-2 text-red-500 font-semibold px-5 cursor-pointer items-center">
-            <ArrowRightStartOnRectangleIcon className="w-6 h-6 text-red-500 mr-1" /> Sair
-            </div>
+            <ArrowRightStartOnRectangleIcon className="w-6 h-6 text-red-500 mr-1" />{" "}
+            Sair
+          </div>
         </div>
       ) : (
         ""
       )}
-      </div>
+    </div>
   );
 };
 
